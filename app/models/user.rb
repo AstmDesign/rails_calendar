fafdc5ae1fbe87d4701d409a0ee7b625d2aa4bf4
Astm <astm_desig@hotmail.com>
2018-12-02 16:07:39 +0200

    login with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
-  attr_accessor :password
+  attr_accessor :password, :password_confirmation
 
   # Scopes
   default_scope { order(id: :desc) }
@@ -13,13 +13,25 @@ class User < ActiveRecord::Base
   # validates :password, length: { minimum: 8 }
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.name = auth.info.name
-      user.uid = auth.uid
-      user.provider = auth.provider
-      user.image = auth.info.image
+    # pass = Devise.friendly_token[0,20]
+    #
+    # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    #   user.email = auth.info.email
+    #   user.password = pass
+    #   user.name = auth.info.name
+    #   user.uid = auth.uid
+    #   user.provider = auth.provider
+    #   user.image = auth.info.image
+    # end
+
+    user = User.where(:provider => auth.provider).where(:email => auth.info.email).first
+
+    unless user
+      pass = Devise.friendly_token[0,20]
+      user = User.create(name: auth.info.name, email: auth.info.email, provider: auth.provider, image: auth.info.image, password: pass, password_confirmation: pass)
     end
+
+    user
   end
 
 end
